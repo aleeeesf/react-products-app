91d30586ac960c516fed72f76ef73813ae8b60e0
Alejandro <alejandroserfer@gmail.com>
2026-05-13 00:58:27 +0200

    feat: added api calls functions

---
+++ src/api/product.api.ts
@@ -0,0 +1,17 @@
+const BASE_URL = 'https://itx-frontend-test.onrender.com/api';
+
+export async function fetchListProducts(): Promise<any> {
+    const response = await fetch(`${BASE_URL}/products`);
+    if (!response.ok) {
+        throw new Error('Failed to fetch products');
+    }
+    return response.json();
+}
+
+export async function fetchProductDetail(id: string): Promise<any> {
+    const response = await fetch(`${BASE_URL}/products/${id}`);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch product with id ${id}`);
+    }
+    return response.json();
+}
