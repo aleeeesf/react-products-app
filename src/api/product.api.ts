ef6d2dd2373c4278f45f24e264ac7653f685b8ce
Alejandro <alejandroserfer@gmail.com>
2026-05-13 02:33:51 +0200

    fix: product api url

--- src/api/product.api.ts
+++ src/api/product.api.ts
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://itx-frontend-test.onrender.com/api';
 
 export async function fetchListProducts(): Promise<any> {
-    const response = await fetch(`${BASE_URL}/products`);
+    const response = await fetch(`${BASE_URL}/product`);
     if (!response.ok) {
         throw new Error('Failed to fetch products');
     }
@@ -9,7 +9,7 @@ export async function fetchListProducts(): Promise<any> {
 }
 
 export async function fetchProductDetail(id: string): Promise<any> {
-    const response = await fetch(`${BASE_URL}/products/${id}`);
+    const response = await fetch(`${BASE_URL}/product/${id}`);
     if (!response.ok) {
         throw new Error(`Failed to fetch product with id ${id}`);
     }
